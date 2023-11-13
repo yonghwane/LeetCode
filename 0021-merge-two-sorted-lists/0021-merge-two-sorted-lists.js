b2502acbbfb1008a55a907a348a8b363dd6b6396
yonghwane <149296072+yonghwane@users.noreply.github.com>
2023-11-13 13:31:27 +0900

    Time: 53 ms (82.96%), Space: 44.6 MB (12.91%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -10,7 +10,6 @@ var mergeTwoLists = function(list1, list2) {
     var mergedList = new ListNode();
     var current = mergedList;
 
-    // list1とlist2の値を比較しながら新しいリストを構築
     while (list1 !== null && list2 !== null) {
         if (list1.val < list2.val) {
             current.next = list1;
@@ -22,14 +21,12 @@ var mergeTwoLists = function(list1, list2) {
         current = current.next;
     }
 
-    // どちらかのリストが残っている場合、残りの要素を追加
     if (list1 !== null) {
         current.next = list1;
     } else {
         current.next = list2;
     }
 
-    // 最初のダミーノードをスキップして結果を返す
     return mergedList.next;
 };
 
